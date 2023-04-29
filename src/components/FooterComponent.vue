01cd1b1dc1b649c9dd7e0148615a8a86fb64728d
giospes <giovannijacopo.spes@gmail.com>
2023-04-29 21:21:30 +0200

    done

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,23 +1,72 @@
 <template>
-    <footer class="wrapper">
-
+    <footer class="pt-5">
+        <div class="up">
+            <h4>Subscribe to our newsletter</h4>
+            <input class="w-100" type="email">
+        </div>
+        <div class="down py-5 d-flex justify-content-center gap-5">
+            <div class="p-3">
+                <h2>Explore</h2>
+                <ul>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                </ul>
+            </div>
+            <div class="p-3">
+                <h2>Support Us</h2>
+                <ul>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                </ul>
+            </div>
+            <div class="p-3">
+                <h2>Useful Links</h2>
+                <ul>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                </ul>
+            </div>
+            <div class="p-3">
+                <h2>Contact Us</h2>
+                <ul>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                    <li>Accommodation</li>
+                </ul>
+            </div>
+        </div>
     </footer>
 </template>
 
 <script>
-    import {store} from '../data/store';
     export default {
-        data(){
-            return{
-                store
-            }
-        }
+        
     }
 </script>
 
 <style lang="scss" scoped>
     footer{
-        height: 300px;
-        background-color: aqua;
+        background-color: #192735 ;
+        color: white;
+    }
+    .up{
+        text-align: center;
+        width: 600px;
+        margin: 0 auto;
     }
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,10 +1,10 @@
 <template>
     <header class="position-fixed">
-        <div class="wrapper h-100 d-flex justify-content-between">
-            <img id="logo" src="./img/Logo.jpg" alt="logo" class="d-none d-sm-block">
+        <div class="wrapper d-flex w-100 h-100">
+            <img id="logo" src="../../public/img/Logo.jpg" alt="logo" class=" d-none d-sm-block">   
             <ul class="d-flex align-items-center justify-content-end gap-4 h-100">
                 <li v-for="(section, index) in store.sections" :key="index"><a :href="'#' + section" class="fw-bold">{{ section }}</a></li>
-            </ul>   
+            </ul>  
         </div>
     </header>
 </template>
@@ -27,8 +27,18 @@
         z-index: 100;
         background-color: white;
 
+
+
         #logo{  
             height: 80px
+        }
+        .wrapper{
+            justify-content: space-between        
+        }
+        @media screen and (max-width: 568px){
+            .wrapper{
+                justify-content: center         
+            }
         }
     }
 
